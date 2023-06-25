debe03670ebe9b4e308f5e589269ec1eba836106
NSergeevich87 <niknikitaser@gmail.com>
2023-06-25 15:58:36 +0200

    addfiles

---
+++ src/main/java/ru/gb/api/nsergeevich/lection_1/Program.java
@@ -0,0 +1,52 @@
+package ru.gb.api.nsergeevich.lection_1;
+
+import java.util.Scanner;
+
+public class Program {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        printName();
+        task2();
+        task3("Vvedenie po kursu");
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+
+    }
+
+    static  void task3(String str){
+        String[] words = str.split(" ");
+        for (int i = words.length - 1; i >= 0; i--){
+            System.out.printf("%s", words[i]);
+        }
+    }
+
+    static void printName(){
+        int i = 12;
+        double j = 5.5;
+
+        System.out.println("Vvedite imya: ");
+        String name = scanner.nextLine();
+        System.out.println("Privet, " + name + "!");
+        System.out.printf("Privet, %s ! %d %f\n", name, i, j);
+    }
+
+    static  void  task2(){
+        int counter = 0;
+        int max = 0;
+
+        int[] array1 = new int[5];
+        int[] array2 = {1, 0, 0, 1, 1, 1, 0, 1};
+        for (int i : array2) {
+            if (i == 1)
+                counter++;
+            else {
+                if (counter > max)
+                    max = counter;
+                counter = 0;
+            }
+        }
+        System.out.printf("");
+    }
+}
