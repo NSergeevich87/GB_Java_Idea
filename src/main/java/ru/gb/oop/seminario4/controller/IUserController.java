5720f17b6d1d1bc9841fc127b4690bd1f3599285
NSergeevich87 <niknikitaser@gmail.com>
2023-08-02 10:21:01 +0200

    deberes6

--- src/main/java/ru/gb/oop/seminario4/controller/IUserController.java
+++ src/main/java/ru/gb/oop/seminario4/controller/IUserController.java
@@ -3,5 +3,9 @@ package ru.gb.oop.seminario4.controller;
 import ru.gb.oop.seminario4.data.User;
 
 public interface IUserController <T extends User>{
+    /**
+     * Принцип единственной ответственности, данный интерфейс выполняет только одну функцию создать юзера.
+     * Принцип разделения интерфейса (ISP)
+     */
     void create(String firstName, String surName, String lastName);
 }
