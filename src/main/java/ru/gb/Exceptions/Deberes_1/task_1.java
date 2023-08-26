fc347f262393d1dd0685d7f7b898d036c4397b96
NSergeevich87 <niknikitaser@gmail.com>
2023-08-26 13:04:38 +0200

    deberes_exeption_1

---
+++ src/main/java/ru/gb/Exceptions/Deberes_1/task_1.java
@@ -0,0 +1,10 @@
+package ru.gb.Exceptions.Deberes_1;
+
+public class task_1 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        for (int i = 0; i < 7; i++) {
+            array[i] = i + 1;
+        }
+    }
+}
