50b6390a1897a12a807dea8e418749f8a7b255e0
NSergeevich87 <niknikitaser@gmail.com>
2024-04-16 17:39:45 +0200

    semi2_16/04/24

--- src/main/Test/ru/gb/PerfectGBTests.java
+++ src/main/Test/ru/gb/PerfectGBTests.java
@@ -1,21 +1,31 @@
 package ru.gb;
 
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.gb.pom.LoginPage;
+import ru.gb.pom.MainPage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PerfectGBTests {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    // add login and page
+    // ====================
+    private LoginPage loginPage;
+    private MainPage mainPage;
+    // ====================
     private static String USERNAME;
     private static String PASSWORD;
     @BeforeAll
@@ -27,28 +37,56 @@ public class PerfectGBTests {
     @BeforeEach
     public void setupTest() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-    @Test
-    void loginTest() {
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://test-stand.gb.ru/login");
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.cssSelector("form#login input[type='text']"))).sendKeys(USERNAME);
-        wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.cssSelector("form#login input[type='password']"))).sendKeys(PASSWORD);
+        // add login page object
+        loginPage = new LoginPage(driver, wait);
 
-        WebElement loginButton = driver.findElement(By.cssSelector("form#login button"));
-        loginButton.click();
-        wait.until(ExpectedConditions.invisibilityOf(loginButton));
+    }
+//    @Test
+//    void loginTest() {
+//        loginPage.login(USERNAME, PASSWORD);
+//        mainPage = new MainPage(driver, wait);
+//        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+//    }
 
-        WebElement usernameLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.partialLinkText(USERNAME)));
-        assertEquals(String.format("Hello, %s", USERNAME), usernameLink.getText().replace("\n", " ").trim());
+    @Test
+    public void testAddingGroupOnMainPage() { //throws IOException {
+        loginPage.login(USERNAME, PASSWORD);
+        mainPage = new MainPage(driver, wait);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+
+        String groupTestName = "New Test Group " + System.currentTimeMillis();
+        mainPage.createGroup(groupTestName);
+
+        assertTrue(mainPage.waitAndGetGroupTitleByText(groupTestName).isDisplayed());
+
+        // Делаем скриншот
+//        byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+//        Files.write(Path.of(
+//                "src/main/Test/resources/screenshot_" + System.currentTimeMillis() + ".png"), screenshotBytes);
     }
 
+    @Test
+    void testArchiveGroupOnMainPage() {
+        loginPage.login(USERNAME, PASSWORD);
+        mainPage = new MainPage(driver, wait);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+
+        String groupTestName = "New Test Group " + System.currentTimeMillis();
+        mainPage.createGroup(groupTestName);
+
+        mainPage.closeCreateGroupModalWindow();
+
+        assertEquals("active", mainPage.getStatusOfGroupWithTitle(groupTestName));
+        mainPage.clickTrashIconOnGroupWithTitle(groupTestName);
+        assertEquals("inactive", mainPage.getStatusOfGroupWithTitle(groupTestName));
+        mainPage.clickRestoreFromTrashIconOnGroupWithTitle(groupTestName);
+        assertEquals("active", mainPage.getStatusOfGroupWithTitle(groupTestName));
+    }
     @AfterEach
     public void teardown() {
         driver.quit();
---
+++ src/main/Test/ru/gb/pom/MainPage.java
@@ -0,0 +1,81 @@
+package ru.gb.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.gb.pom.elements.GroupTableRow;
+
+import java.util.List;
+
+public class MainPage {
+    private final WebDriverWait wait;
+
+    @FindBy(css = "nav li.mdc-menu-surface--anchor a")
+    private WebElement usernameLinkInNavBar;
+
+    @FindBy(id = "create-btn")
+    private WebElement createGroupButton;
+
+    @FindBy(xpath = "//form//span[contains(text(), 'Group name')]/following-sibling::input")
+    private WebElement groupNameField;
+
+    @FindBy(css = "form div.submit button")
+    private WebElement submitButtonOnModalWindow;
+
+    @FindBy(xpath = "//span[text()='Creating Study Group']" +
+            "//ancestor::div[contains(@class, 'form-modal-header')]//button")
+    private WebElement closeCreateGroupIcon;
+
+    @FindBy(xpath = "//table[@aria-label='Tutors list']/tbody/tr")
+    private List<WebElement> rowsInGroupTable;
+
+    public MainPage(WebDriver driver, WebDriverWait wait) {
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement waitAndGetGroupTitleByText(String title) {
+        String xpath = String.format("//table[@aria-label='Tutors list']/tbody//td[text()='%s']", title);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
+    public void createGroup(String groupName) {
+        wait.until(ExpectedConditions.visibilityOf(createGroupButton)).click();
+        wait.until(ExpectedConditions.visibilityOf(groupNameField)).sendKeys(groupName);
+        submitButtonOnModalWindow.click();
+        waitAndGetGroupTitleByText(groupName);
+    }
+
+    public void closeCreateGroupModalWindow() {
+        closeCreateGroupIcon.click();
+        wait.until(ExpectedConditions.invisibilityOf(closeCreateGroupIcon));
+    }
+
+    public String getUsernameLabelText() {
+        return wait.until(ExpectedConditions.visibilityOf(usernameLinkInNavBar))
+                .getText().replace("\n", " ");
+    }
+
+    public void clickTrashIconOnGroupWithTitle(String title) {
+        getRowByTitle(title).clickTrashIcon();
+    }
+
+    public void clickRestoreFromTrashIconOnGroupWithTitle(String title) {
+        getRowByTitle(title).clickRestoreFromTrashIcon();
+    }
+
+    public String getStatusOfGroupWithTitle(String title) {
+        return getRowByTitle(title).getStatus();
+    }
+
+    private GroupTableRow getRowByTitle(String title) {
+        return rowsInGroupTable.stream()
+                .map(GroupTableRow::new)//.map(root -> new GroupTableRow(root))//.map(GroupTableRow::new)
+                .filter(row -> row.getTitle().equals(title))
+                .findFirst().orElseThrow();
+    }
+}
