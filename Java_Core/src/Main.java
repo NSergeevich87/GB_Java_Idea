9478db3fd9af4f2c8181f84f5e22739d2c276d14
NSergeevich87 <niknikitaser@gmail.com>
2024-05-05 18:37:58 +0200

    hw_1

---
+++ Java_Core/src/Main.java
@@ -0,0 +1,9 @@
+//import SimpleClass.Book;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //Book book = new Book("GovnoBook");
+        //System.out.println(book.getBook_name());
+    }
+}
--- src/main/java/ru/gb/_auto_testing/semi_6/demo/ExampleMain.java
+++
@@ -1,8 +0,0 @@
-package ru.gb._auto_testing.semi_6.demo;
-
-public class ExampleMain {
-
-    public static void main(String[] args) {
-
-    }
-}
